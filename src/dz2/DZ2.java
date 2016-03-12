07487877a3c994ff588ab48e54a793593bd51f5c
Aslan125 <stinger125@mail.ru>
2016-03-12 18:35:00 +0300

    new Commit0

---
+++ src/dz2/DZ2.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package dz2;
+
+/**
+ *
+ * @author Aslan
+ */
+public class DZ2
+{
+    public static void main(String[] args)
+    {
+        System.out.println("DZ2");
+    }
+}
